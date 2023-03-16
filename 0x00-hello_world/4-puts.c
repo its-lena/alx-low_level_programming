7fd00e3b31ea30991bab42e220ca21c5512c3425
its-lena <lenagamal30@gmail.com>
2023-03-16 16:31:06 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
-/*
- * program prints
+/**
+ * main - a program that prints a statement
+ * Return : 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
