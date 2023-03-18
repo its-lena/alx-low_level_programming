fd6b97f277f1596f9c9d1732e46d3e5ff8a78d89
its-lena <lenagamal30@gmail.com>
2023-03-17 18:43:10 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints alpha
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+putchar('\n');
+return (0);
+}
