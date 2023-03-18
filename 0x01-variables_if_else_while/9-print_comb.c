7c48d669cc1bb33d2575a94adacb7a628425c4b6
its-lena <lenagamal30@gmail.com>
2023-03-17 19:06:59 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - print numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 9; i++)
+{
+	putchar(i);
+	putchar(", ");
+}
+putchar(i);
+putchar('\n');
+return (0);
+}
+
