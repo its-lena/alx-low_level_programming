af6cbf0791c148c93c80fba031a75f8660bd1d13
its-lena <lenagamal30@gmail.com>
2023-03-23 16:37:21 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,13 +4,12 @@
  */
 void more_numbers(void)
 {
-int i;
+int i,j;
 for (i = 0; i <= 9; i++)
 {
-int j;
 for (j = 0; j <= 14; j++)
 {
-	if (i >= 10)
+	if (j >= 10)
 	_putchar('1');
 	_putchar(j % 10 + '0');
 }
