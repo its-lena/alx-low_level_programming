49911b34aca8e471a6690353a4a01917820a8288
its-lena <lenagamal30@gmail.com>
2023-03-22 18:44:55 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alpha_x10 - prints alpha
+ */
+void print_alphabet_x10(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+	char c;
+	for (c = 'a'; c <= 'z' ; c++)
+		putchar(c);
+	putchar('\n');
+}
+}
