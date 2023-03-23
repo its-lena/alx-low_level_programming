d418cbf5a35e9259ec631115fc463fd4763a9b02
its-lena <lenagamal30@gmail.com>
2023-03-23 16:27:26 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints many numbers
+ */
+void more_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+char j;
+for (j = '0'; j <= '14'; j++)
+{
+	if (i >= '10')
+	_putchar('1');
+	_putchar(j % 10);
+}
+}
+_putchar('\n');
+}
