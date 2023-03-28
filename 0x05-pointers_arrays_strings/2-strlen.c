8fd17b944583a9294383ab498f502d997e0f2a8f
its-lena <lenagamal30@gmail.com>
2023-03-27 17:53:12 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longg = 0;
+
+	while (*s != '\0')
+	{
+		longg++;
+		s++;
+	}
+
+	return (longg);
+}
+
