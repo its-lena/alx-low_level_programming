a2c9e4c5d588f7a2b8a7461f9ca23bdcd6b9765b
its-lena <lenagamal30@gmail.com>
2023-04-15 02:14:56 -0700

    commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - prints number factorial
+ * @n: input
+ * Return: result
+ */
+int factorial(int n)
+{
+if (n <= 1)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
